cd834c2dd484c0e2c4cb8a8097578a17d207d758
MrBoosh <timon.haupt@hotmail.de>
2023-08-11 11:13:11 +0200

    Update

---
+++ PackagesAndImports/AnotherPackage/ImportExample.java
@@ -0,0 +1,16 @@
+package PackagesAndImports.AnotherPackage;
+
+import java.util.Random;
+
+/**
+ * @author Timon 11/08/2023
+ */
+public class ImportExample {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		System.out.println(random.nextInt(5));
+
+	}
+
+}
