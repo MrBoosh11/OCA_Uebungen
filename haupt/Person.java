d4c880dec50a81788fc89dff8b4238b6732b21b5
MrBoosh <timon.haupt@hotmail.de>
2023-08-11 10:39:06 +0200

    inital

---
+++ haupt/Person.java
@@ -0,0 +1,19 @@
+package haupt;
+
+/**
+ * @author Timon on 11/08/2023.
+ */
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
