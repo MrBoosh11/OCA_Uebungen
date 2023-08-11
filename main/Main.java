40ea9f6200a6b6ac9472021364d3499b5c43d990
MrBoosh <timon.haupt@hotmail.de>
2023-08-11 10:51:41 +0200

    Update

--- main/Main.java
+++ main/Main.java
@@ -1,13 +1,39 @@
 package main;
 
+/**
+ * This is main method.
+ * 
+ * @author Timon 11/08/2023
+ */
+
 public class Main {
 	
 	public static void main(String[] args) {
-		
+		// printing size of arguments
+		//
+		//
 		System.out.println("args-size= " + args.length);
+		
+		/*
+		 * printing arguments
+		 * another line
+		 */
 		
 		for (int i = 0; i < args.length; i++) {
 			System.out.println("args [" + i + "]=" + args[i]);
 		}
+		
+		sum(5, 6);
+	}
+	
+	/**
+	 * Calculates sum of two integers.
+	 * 
+	 * @param a operand
+	 * @param b operand
+	 * @return sum
+	 */
+	public static int sum(int a, int b ) {
+		return a + b;
 	}
 }
